9f32a92e8d870580d4d96ea65f7064e4830dc281
amdark77 <amdark77@gmail.com>
2016-01-09 21:54:15 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -24,32 +24,4 @@ do
  
   },
   run = run
- }    --https://github.com/amdark77
-    --Telegram.me/AmirDark
-do
- function run(msg, matches)
- 
-  local fuse = '📌 New #feedback\n\n👤 ID : ' .. msg.from.id .. '\n\n🔍 Name: ' .. msg.from.print_name ..'\n\n🈯️ Username: @' .. msg.from.username .. '\n\n 📝 The Pm:\n' .. matches[1] 
- local fuses = '!printf user#id' .. msg.from.id
- 
- 
-   local text = matches[1]
-   local chat = "chat#id"..90312082 
-   --like : local chat = "chat#id"..12345678
-   
-  local sends = send_msg(chat, fuse, ok_cb, false)
-  return '✅ پیغام شما ارسال شد ✅'
- 
- end
- end
- return {
-  
-  description = "Feedback",
- 
-  usage = "!feedback message",
-  patterns = {
-  "^[!/][Ff]eedback (.*)$"
- 
-  },
-  run = run
  }
