9b683eb7480b9b66d6e7269b5f4a74beadf17de3
amdark77 <amdark77@gmail.com>
2016-01-10 10:49:41 +0330

    Update autoleave.lua

--- plugins/autoleave.lua
+++ plugins/autoleave.lua
@@ -1,31 +1,17 @@
---[[
-Kicking ourself (bot) from unmanaged groups.
-When someone invited this bot to a group, the bot then will chek if the group is in its moderations (moderation.json). If not, the bot will exit immediately by kicking itself out of that group.
-No switch available. You need to turn it on or off using !plugins command.
-Testing needed.
---]]
-
--- suppress '*** lua: attempt to call a nil value' warning
-local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
-end
-
 local function run(msg)
-  if msg.service and msg.action.type == 'chat_add_user' then
-    local data = load_data(_config.moderation.data)
-    if not data[tostring(msg.to.id)] then
-      print "This is not our group. Leaving..."
-      chat_del_user('chat#id'..msg.to.id, 'user#id'..our_id, callback, false)
-    end
-  end
+local bot_id = 165943175
+if msg.action and msg.action.type then
+if msg.action.type == 'chat_add_user' and msg.action.user.id == bot_id then
+chat_del_user('chat#id'..msg.to.id, 'user#id'..bot_id, ok_cb, true)
+end
+end
 end
 
 return {
-  description = "Kicking ourself (bot) from unmanaged groups.",
-  usage = "No switch available. Turn it on or off using !plugins command.",
-  patterns = {
-    "^!!tgservice (.+)$"
-  },
-  run = run
+description = "Plugin to delete him self from groups.", 
+usage = {},
+patterns = {"^!!tgservice (.+)$"}, 
+run = run
 }
+-- https://github.com/amdark77
+-- https://telegram.me/AmirDark
